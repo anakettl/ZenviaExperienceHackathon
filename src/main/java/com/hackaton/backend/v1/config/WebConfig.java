25d25b0feeba713ec3a6ff6ebcf154d580a5e039
analuciabolico <51336956+analuciabolico@users.noreply.github.com>
2020-07-19 17:45:22 -0300

    update Cors

--- src/main/java/com/hackaton/backend/v1/config/WebConfig.java
+++ src/main/java/com/hackaton/backend/v1/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.hackaton.backend.v1.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
